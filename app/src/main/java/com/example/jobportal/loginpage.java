4cd78404b4b6809de489966fb9577b6e59ace1cc
Athmik <athmik1382001@gmail.com>
2022-06-07 06:42:34 +0530

    .

--- app/src/main/java/com/example/jobportal/loginpage.java
+++ app/src/main/java/com/example/jobportal/loginpage.java
@@ -1,18 +1,29 @@
 package com.example.jobportal;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class loginpage extends AppCompatActivity {
     private Button signup;
-    private EditText email;
-    private EditText password;
+    private EditText nEmail;
+    private EditText nPassword;
     private Button login;
+    ProgressBar progress;
+    FirebaseAuth mAuth;
 
 
     @Override
@@ -20,28 +31,56 @@ public class loginpage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loginpage);
         getSupportActionBar().hide();
-        signup=findViewById(R.id.signupbtn2);
+        nEmail = findViewById(R.id.editTextTextEmailAddress);
+        nPassword = findViewById(R.id.editTextTextPassword);
+        login = findViewById(R.id.loginbtn2);
+        progress=findViewById(R.id.progressBar);
+        mAuth=FirebaseAuth.getInstance();
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=nEmail.getText().toString().trim();
+                String password=nPassword.getText().toString().trim();
+                if(TextUtils.isEmpty(password)){
+                    nPassword.setError("Required Field...");
+                    return;
+                }
+                if(TextUtils.isEmpty(email)){
+                    nEmail.setError("Required Field...");
+                    return;
+                }
+
+
+                progress.setVisibility(View.VISIBLE);
+                //user autentication
+                mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(loginpage.this, "Login successfull", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),homeactivity.class));
+                        }else {
+                            Toast.makeText(loginpage.this, "Wrong Details!!!"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progress.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+
+        signup = findViewById(R.id.signupbtn2);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(loginpage.this,signup.class);
+                Intent intent = new Intent(loginpage.this, signup.class);
                 startActivity(intent);
             }
         });
 
 
     }
-    private void LoginFuntion(){
-        email=findViewById(R.id.editTextTextEmailAddress);
-        password=findViewById(R.id.editTextTextPassword);
-        login=findViewById(R.id.loginbtn2);
-
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-    }
 }
+
+
+
--- app/src/main/java/com/example/jobportal/signup.java
+++ app/src/main/java/com/example/jobportal/signup.java
@@ -9,6 +9,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,10 +18,13 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class signup extends AppCompatActivity {
-    private Button loginbtn;
-    private EditText email;
-    private EditText password;
-    private Button signupbtn;
+     Button loginbtn,signupbtn;
+    EditText memail,mpassword,mname;
+    ProgressBar progress;
+    FirebaseAuth mAuth;
+
+
+
 
 
 
@@ -29,6 +33,56 @@ public class signup extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
         getSupportActionBar().hide();
+        mname=findViewById(R.id.editTextName);
+        memail=findViewById(R.id.editTextTextEmailAddress2);
+        mpassword=findViewById(R.id.editTextTextPassword2);
+        signupbtn=findViewById(R.id.signupbtn3);
+        progress=findViewById(R.id.progressBar2);
+
+        mAuth=FirebaseAuth.getInstance();
+
+
+
+        signupbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email=memail.getText().toString().trim();
+                String password=mpassword.getText().toString().trim();
+                String name=mname.getText().toString().trim();
+                if(TextUtils.isEmpty(name)){
+                    mpassword.setError("Required Field...");
+                    return;
+                }
+
+                if(TextUtils.isEmpty(password)){
+                    mpassword.setError("Required Field...");
+                    return;
+                }
+                if(TextUtils.isEmpty(email)){
+                    memail.setError("Required Field...");
+                    return;
+                }
+
+                if(password.length()<6){
+                    mpassword.setError("Password must be atleast 6 characters");
+                    return;
+                }
+                progress.setVisibility(View.VISIBLE);
+                //firebase registration
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                      if(task.isSuccessful()){
+                          Toast.makeText(signup.this, "Account Created", Toast.LENGTH_SHORT).show();
+                          startActivity(new Intent(getApplicationContext(),homeactivity.class));
+                      }else{
+                          Toast.makeText(signup.this, "Error!!!"+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                          progress.setVisibility(View.GONE);
+                      }
+                    }
+                });
+            }
+        });
 
 
         loginbtn=findViewById(R.id.loginbtn4);
