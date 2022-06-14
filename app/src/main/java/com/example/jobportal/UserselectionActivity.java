0202a2be8b978d60178886ad9ce7bb7a7a47851f
Athmik <athmik1382001@gmail.com>
2022-06-14 06:14:00 +0530

    14-06-2022

---
+++ app/src/main/java/com/example/jobportal/UserselectionActivity.java
@@ -0,0 +1,29 @@
+package com.example.jobportal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class UserselectionActivity extends AppCompatActivity {
+    Button hirer,seeker;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.userselection);
+        getSupportActionBar().hide();
+        seeker=findViewById(R.id.seekerbtn);
+        hirer=findViewById(R.id.hirerbtn);
+        seeker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(UserselectionActivity.this,loginpage.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
